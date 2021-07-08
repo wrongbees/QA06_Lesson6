dd1a73f6406f9c8d74efa4fdc4cfbaddbc360954
Vladimir <mkking@gmail.com>
2021-07-08 12:41:41 +0300

    исправлен

--- src/test/java/postman/models/Milestone.java
+++ src/test/java/postman/models/Milestone.java
@@ -1,5 +1,6 @@
 package postman.models;
 
+import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
 
@@ -7,14 +8,14 @@ public class Milestone {
     public int id = 19;
     public String name = "MS_01";
     public String  description = null;
-    public Date start_on = null;
-    public Date started_on = new Date(1625650876);
+    public Timestamp start_on = null;
+    public Timestamp started_on = new Timestamp(1625650876);
     public boolean is_started = true;
-    public Date due_on = null;
+    public Timestamp due_on = null;
     public boolean is_completed = false;
-    public Date completed_on = null;
+    public Timestamp completed_on = null;
     public int project_id = 24;
-    public int parent_id = 0;
+    public int parent_id;
     public String refs = null;
     public String url = "https://aqa06onl01.testrail.io/index.php?/milestones/view/19";
     public List<Object> milestones;
--- src/test/java/postman/models/Project.java
+++ src/test/java/postman/models/Project.java
@@ -7,7 +7,7 @@ public class Project {
     public String announcement = "This is the description for the project, Test_added_Vladimir";
     public boolean show_announcement = true;
     public boolean is_completed = false;
-    public int completed_on = 0;
+    public int completed_on ;
     public Integer suite_mode = 3;
     public String url = "https://aqa06onl01.testrail.io/index.php?/projects/overview/24";
 
